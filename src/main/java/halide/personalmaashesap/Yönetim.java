f5925b624886a0888a011529d44299c22b0d50a4
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-26 13:26:38 +0100

    first commit

---
+++ src/main/java/halide/personalmaashesap/Yönetim.java
@@ -0,0 +1,10 @@
+package halide.personalmaashesap;
+
+public class Yönetim { public static void main(String[] args) {
+    Personel pers1=new Personel("hasan ali",2500,40,2016);
+pers1.netMaasGöster();
+    Personel pers2=new Personel("Ridvan ",3000,30,2010);
+    pers2.netMaasGöster();
+
+}
+}
---
+++ src/main/java/halide/personalmaashesap/deneme.java
@@ -0,0 +1,10 @@
+package halide.personalmaashesap;
+
+public class deneme {
+    public static void main(String[] args) {
+       Personel pers=new Personel();
+        pers.keyfi();
+
+
+    }
+}
